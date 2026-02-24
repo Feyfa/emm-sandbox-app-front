b07392f4b5c65b5591dec1c820d45c688a3b8811
Muhammad Jidan <muhammadjidan703@gmail.com>
2026-02-24 21:43:33 +0700

    fix: change npm install to npm ci

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,16 +2,16 @@
 
 # Cek apakah node_modules sudah ada
 if [ ! -d "node_modules" ]; then
-  echo "node_modules tidak ditemukan, menjalankan npm install --legacy-peer-deps..."
-  npm install --legacy-peer-deps
+  echo "node_modules tidak ditemukan, menjalankan npm ci --legacy-peer-deps..."
+  npm ci --legacy-peer-deps
 elif [ "package-lock.json" -nt "node_modules" ] || [ "package.json" -nt "node_modules" ]; then
   echo "package.json atau package-lock.json lebih baru dari node_modules."
   if [ -d "node_modules" ]; then
     echo "Menghapus node_modules lama sebelum install ulang..."
     rm -rf node_modules
   fi
-  echo "Menjalankan npm install --legacy-peer-deps..."
-  npm install --legacy-peer-deps
+  echo "Menjalankan npm ci --legacy-peer-deps..."
+  npm ci --legacy-peer-deps
 else
   echo "node_modules sudah ada dan up-to-date, melewati npm install"
 fi
