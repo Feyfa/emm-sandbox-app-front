822cb8908d9501468cd89ba6d398e07ee97b7406
Muhammad Jidan <muhammadjidan703@gmail.com>
2026-02-24 21:57:17 +0700

    ngga tau ah

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,13 +7,13 @@ if [ ! -d "node_modules" ]; then
 elif [ "package-lock.json" -nt "node_modules" ] || [ "package.json" -nt "node_modules" ]; then
   echo "package.json atau package-lock.json lebih baru dari node_modules."
   if [ -d "node_modules" ]; then
-    echo "Menghapus node_modules lama sebelum install ulang..."
+    echo "Menghapus node_modules lama sebelum menjalankan npm ci ulang..."
     rm -rf node_modules
   fi
   echo "Menjalankan npm ci --legacy-peer-deps..."
   npm ci --legacy-peer-deps
 else
-  echo "node_modules sudah ada dan up-to-date, melewati npm install"
+  echo "node_modules sudah ada dan up-to-date, melewati npm ci"
 fi
 
 # Jalankan dev server
